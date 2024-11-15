5c043b7d33136a797c48dad944bfd4769e37aebe
六元 <liuyuans@live.com>
2024-11-15 16:24:38 +0800

    cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-
+        $schedule->command('XiaomiSports 15138674502 liuyuan321');
     }
 
     /**

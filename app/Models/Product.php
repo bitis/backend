d5faa381d8f226fa9af641ea1c1436d06bd5e5d7
六元 <liuyuans@live.com>
2024-12-26 18:29:49 +0800

    text

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Traits\DefaultDatetimeFormat;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -46,6 +47,8 @@ class Product extends Model
     protected $casts = [
         'images' => 'array',
     ];
+
+    protected $appends = ['type_text'];
 
     public function getTypeTextAttribute(): string
     {

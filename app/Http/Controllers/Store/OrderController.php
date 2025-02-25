46a8787050a657a9b49d549e7de2c879561de866
六元 <liuyuans@live.com>
2025-02-25 16:11:01 +0800

    member order

--- app/Http/Controllers/Store/OrderController.php
+++ app/Http/Controllers/Store/OrderController.php
@@ -19,10 +19,11 @@ class OrderController extends Controller
     {
         $orders = Order::with(['products', 'member:id,avatar,name,mobile'])
             ->where('store_id', $this->store_id)
+            ->when($request->input('member_id'), function ($query, $member_id) {
+                $query->where('member_id', $member_id);
+            })
             ->when($request->input('keywords'), function ($query, $keywords) {
-                $query->where('order_no', 'like', "%{$keywords}%")
-                    ->orWhere('name', 'like', "%{$keywords}%")
-                    ->orWhere('mobile', 'like', "%{$keywords}%");
+                $query->where('order_number', 'like', "%{$keywords}%");
             })
             ->orderBy('id', 'desc')
             ->paginate(getPerPage());

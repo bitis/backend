d2e452f32d660361f4f3cb013956e9a5f165f670
刘源 <liuyuans@live.com>
2025-04-02 21:21:36 +0800

    We Bank

--- routes/financial.php
+++ routes/financial.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::namespace('Financial')->prefix('financial')->group(function () {
     Route::prefix('we-bank')->group(function () {
-        Route::post('index', 'WeBankController@index');
+        Route::post('products', 'WeBankController@index');
         Route::post('calendar', 'WeBankController@calendar');
     });
 });

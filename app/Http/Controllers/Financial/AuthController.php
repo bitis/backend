db813171382479f0c2f40090551d0d5ed907c402
六元 <liuyuans@live.com>
2025-04-12 11:03:55 +0800

    Login

--- app/Http/Controllers/Financial/AuthController.php
+++ app/Http/Controllers/Financial/AuthController.php
@@ -3,14 +3,30 @@
 namespace App\Http\Controllers\Financial;
 
 use App\Http\Controllers\Controller;
+use App\Models\MiniUser;
+use EasyWeChat\Work\MiniProgram\Application;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
     public function login(Request $request): JsonResponse
     {
+        $app = new Application(config('wechat.finance'));
 
-        return success();
+        $session = $app->getUtils()->codeToSession($request->input('code'));
+
+        if (empty($session)) {
+            return fail('获取 session 失败');
+        }
+
+        $user = MiniUser::firstOrCreate([
+            'openid' => $session['openid'],
+        ], [
+            'token' => Str::random('16'),
+        ]);
+
+        return success($user);
     }
 }
--- routes/financial.php
+++ routes/financial.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::namespace('Financial')->prefix('financial')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('login', 'AuthController@login');
+    });
     Route::prefix('we-bank')->group(function () {
         Route::post('products', 'WeBankController@index');
         Route::post('detail', 'WeBankController@detail');

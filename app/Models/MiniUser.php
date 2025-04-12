8c05494c0e026ccc54e28c96ffb1244b889a3d24
六元 <liuyuans@live.com>
2025-04-12 11:49:45 +0800

    Login

--- app/Models/MiniUser.php
+++ app/Models/MiniUser.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class MiniUser extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'openid',
+        'unionid',
+        'token',
+        'coin'
+    ];
 }

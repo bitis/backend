c1767c221e980e86f0fa1e238ca8d870f9e9a55b
六元 <liuyuans@live.com>
2025-04-16 17:15:48 +0800

    兼容

--- app/Http/Controllers/Financial/WeBankController.php
+++ app/Http/Controllers/Financial/WeBankController.php
@@ -72,7 +72,8 @@ class WeBankController extends Controller
             ->orderBy('earnings_rate_date')
             ->get();
 
-        $stock->halfyearyield = $stock->half_year_yield;
+        $stock->halfyearyield = $stock->half_year_yield ?: '--';
+        $stock->season_yield = $stock->season_yield ?: '--';
         $stock->amount = $amount;
         $stock->confirm_date = ''; // 确认日期
         $stock->confirm_value = 0; // 确认净值

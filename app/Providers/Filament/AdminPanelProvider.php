a5236edf234ced573dfc207021ed95dc4dfbbf8e
六元 <liuyuans@live.com>
2025-04-23 11:06:29 +0800

    Control Panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->brandName('Control Panel')
             ->login()
             ->colors([
                 'primary' => Color::Amber,

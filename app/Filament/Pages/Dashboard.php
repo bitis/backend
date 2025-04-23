6292650003d43226024df5c7b7631443e8ee0153
六元 <liuyuans@live.com>
2025-04-23 15:03:29 +0800

    Filament

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+use Filament\Facades\Filament;
+use Filament\Widgets\Widget;
+use Filament\Widgets\WidgetConfiguration;
+
+class Dashboard extends Page
+{
+    protected static string $routePath = '/';
+
+    protected static ?int $navigationSort = -2;
+
+    protected static ?string $title = '仪表盘';
+
+    protected static string $view = 'filament-panels::pages.dashboard';
+
+    protected static ?string $navigationIcon = 'heroicon-m-home';
+
+    public static function getRoutePath(): string
+    {
+        return static::$routePath;
+    }
+
+    /**
+     * @return array<class-string<Widget> | WidgetConfiguration>
+     */
+    public function getWidgets(): array
+    {
+        return Filament::getWidgets();
+    }
+
+    /**
+     * @return array<class-string<Widget> | WidgetConfiguration>
+     */
+    public function getVisibleWidgets(): array
+    {
+        return $this->filterVisibleWidgets($this->getWidgets());
+    }
+
+    /**
+     * @return int | string | array<string, int | string | null>
+     */
+    public function getColumns(): int|string|array
+    {
+        return 2;
+    }
+}

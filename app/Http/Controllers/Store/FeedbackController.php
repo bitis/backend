bfcfd65db896b5d779ac47dbe435e4de6e715440
六元 <liuyuans@live.com>
2025-04-27 15:12:30 +0800

    意见反馈

---
+++ app/Http/Controllers/Store/FeedbackController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Store;
+
+use App\Http\Controllers\Controller;
+use App\Models\Feedback;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class FeedbackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $feedbacks = Feedback::where('store_id', $this->store_id)
+            ->orderBy('created_at', 'desc')
+            ->limit(30)->get();
+
+        return success($feedbacks);
+    }
+
+    public function form(Request $request): JsonResponse
+    {
+        if (Feedback::where('store_id', $this->store_id)->where('status', 0)->count() > 10)
+            return fail('提交次数过多，请稍后再试');
+
+        Feedback::create([
+            'store_id' => $this->store_id,
+            'content' => $request->input('content'),
+            'images' => $request->input('images'),
+        ]);
+
+        return success();
+    }
+}
---
+++ database/migrations/2025_04_27_145722_create_feedback_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->integer('store_id');
+            $table->string('contents');
+            $table->text('images')->nullable();
+            $table->string('reply')->nullable();
+            $table->tinyInteger('status')->default(0)->comment('0:未回复 1:已回复');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedback');
+    }
+};

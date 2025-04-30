f4d519d61ea6ce3c829b2f11ecc5099d1756c51e
六元 <liuyuans@live.com>
2025-04-30 18:30:26 +0800

    短信群发

---
+++ app/Http/Controllers/Store/SmsConfigController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Http\Controllers\Store;
+
+use App\Http\Controllers\Controller;
+use App\Models\CloudFile;
+use App\Models\SmsConfig;
+use App\Models\SmsDetail;
+use App\Models\SmsRecord;
+use App\Models\SmsSignature;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class SmsConfigController extends Controller
+{
+
+    public function detail(): JsonResponse
+    {
+        $config = SmsConfig::firstOrCreate(['store_id' => $this->store_id]);
+        return success($config);
+    }
+
+    /**
+     * 提交记录
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function history(Request $request): JsonResponse
+    {
+        $records = SmsRecord::where('store_id', $this->store_id)
+            ->when($request->input('title'),
+                fn($query, $title) => $query->where('title', 'like', '%' . $title . '%')
+            )
+            ->when($request->input('created_at'),
+                fn($query, $title) => $query->where('created_at', '>', strtotime($title[0]))
+                    ->where('created_at', '<', strtotime($title[1] . ' 23:59:59'))
+            )
+            ->paginate(getPerPage());
+        return success($records);
+    }
+
+    /**
+     * 发送短信
+     */
+    public function form(Request $request): JsonResponse
+    {
+        $content = $request->input('content');
+        $signature = $request->input('signature');
+        $mobiles = $request->input('mobiles');
+        $import_mobiles = [];
+
+        if ($file = $request->input('file')) {
+            $temp = Storage::disk()->get($file);
+        }
+
+        try {
+            DB::beginTransaction();
+            $record = SmsRecord::create([
+                'store_id' => $this->store_id,
+                'title' => $request->input('title'),
+                'content' => $request->input('content'),
+                'signature' => $signature,
+                'file' => $file ?? null,
+                'content_length' => mb_strlen($signature) + mb_strlen($content) + 7,
+                'mobile_count' => count($mobiles) + count($import_mobiles),
+            ]);
+
+            foreach ($mobiles as $mobile) {
+                SmsDetail::create([
+                    'store_id' => $this->store_id,
+                    'sms_record_id' => $record->id,
+                    'mobile' => $mobile,
+                    'source' => 1,
+                    'content' => $signature . $request->input('content') . "，拒收请回复R",
+                ]);
+            }
+            DB::commit();
+        } catch (Exception $exception) {
+            DB::rollBack();
+            throw $exception;
+        }
+
+        return success($record);
+    }
+
+    /**
+     * 批量上传号码
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function upload(Request $request): JsonResponse
+    {
+        $file = $request->file('file');
+
+        if (!$file) return fail('必须上传一个文件');
+
+        $ext = $file->getClientOriginalExtension();
+
+        $fileName = '/uploads/' . date('Ym') . '/sms/' . Str::random(40) . ($ext ? '.' . $ext : '');
+
+        if (Storage::disk('qcloud')->put($fileName, $file->getContent())) {
+            CloudFile::create([
+                'name' => $file->getClientOriginalName(),
+                'path' => $fileName,
+                'size' => $file->getSize(),
+                'type' => $file->getMimeType(),
+                'user_id' => $request->user()->id,
+            ]);
+
+            return success($fileName);
+        }
+
+        return fail('上传失败');
+    }
+
+    /**
+     * 发送明细
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function detailRecord(Request $request): JsonResponse
+    {
+        $record = SmsDetail::where('store_id', $this->store_id)
+            ->when($request->input('record_id'),
+                fn($query, $record_id) => $query->where('sms_record_id', $record_id)
+            )
+            ->when($request->input('mobile'),
+                fn($query, $mobile) => $query->where('mobile', 'like', '%' . $mobile . '%')
+            )
+            ->when($request->input('created_at'),
+                fn($query, $title) => $query->where('created_at', '>', strtotime($title[0]))
+                    ->where('created_at', '<', strtotime($title[1] . ' 23:59:59'))
+            )
+            ->paginate(getPerPage());
+
+        return success($record);
+    }
+
+    public function createSignature(Request $request): JsonResponse
+    {
+        $name = $request->input('name');
+
+        if (SmsSignature::where('store_id', $this->store_id)->where('name', $name)->exists())
+            return fail('短信签名已存在');
+
+        SmsSignature::create([
+            'store_id' => $this->store_id,
+            'name' => $name
+        ]);
+
+        return success();
+    }
+
+    public function getSignatures(): JsonResponse
+    {
+        return success(SmsSignature::where('store_id', $this->store_id)->get());
+    }
+}
---
+++ database/migrations/2025_04_30_113144_create_sms_configs_table.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sms_configs', function (Blueprint $table) {
+            $table->id();
+            $table->string('store_id');
+            $table->boolean('is_new')->default(1)->comment('是否是新用户');
+            $table->integer('balance')->default(0)->comment('短信数量');
+            $table->boolean('consume_switch')->default(false)->comment('是否开启短信消费');
+            $table->timestamps();
+        });
+
+        Schema::create('sms_packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('套餐名称');
+            $table->integer('number')->comment('短信数量');
+            $table->decimal('price')->comment('价格');
+            $table->decimal('unit_price', 8, 3)->comment('单价');
+            $table->decimal('original_price')->comment('原价');
+            $table->integer('limit')->default(0)->comment('限制购买次数');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('sms_orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('store_id');
+            $table->string('name')->comment('套餐名称');
+            $table->string('order_no')->comment('订单号');
+            $table->integer('number')->comment('短信数量');
+            $table->decimal('price')->comment('价格');
+            $table->string('payment_channel')->nullable()->comment('支付渠道');
+            $table->timestamp('paid_at')->nullable()->comment('支付时间');
+            $table->timestamp('refunded_at')->nullable()->comment('退款时间');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('sms_logs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('store_id');
+            $table->string('remark')->comment('备注');
+            $table->tinyInteger('type')->comment('类型 1:消费通知 2:订单变动');
+            $table->string('order_id')->nullable()->comment('订单ID');
+            $table->string('record_id')->nullable()->comment('发送记录ID');
+            $table->integer('number')->comment('变动数量');
+            $table->integer('balance')->comment('剩余数量');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sms_configs');
+    }
+};

a49b2af86ac82e418d6feb53a6a549b9320e2c9f
刘源 <liuyuans@live.com>
2025-05-01 14:09:47 +0800

    短信管理

--- routes/store.php
+++ routes/store.php
@@ -177,8 +177,8 @@ Route::namespace('Store')->prefix('store')->group(function () {
                 Route::post('destroy', 'PrinterController@destroy');
             });
             Route::prefix('sms')->group(function () { // 短信管理
-                Route::post('detail', 'PrinterController@index');
-                Route::post('form', 'PrinterController@form');
+                Route::post('detail', 'SmsConfigController@index');
+                Route::post('form', 'SmsConfigController@form');
             });
         });
 

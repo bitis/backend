87c3d14c80dd292560ed8263fcde6d5142d3fe93
六元 <liuyuans@live.com>
2025-05-06 11:00:59 +0800

    库存

--- app/Http/Controllers/Store/DashboardController.php
+++ app/Http/Controllers/Store/DashboardController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Store;
 
 use App\Http\Controllers\Controller;
+use App\Models\Member;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\StockWarningConfig;
 use Illuminate\Http\JsonResponse;
 
 class DashboardController extends Controller
@@ -17,6 +21,27 @@ class DashboardController extends Controller
             ['x' => '2024-01-05', 'y' => rand(1,100)],
             ['x' => '2024-01-06', 'y' => rand(1,100)],
             ['x' => '2024-01-07', 'y' => rand(1,100)],
+        ]);
+    }
+
+    /**
+     * App首页数据
+     *
+     * @return JsonResponse
+     */
+    public function appIndex(): JsonResponse
+    {
+        $stat = [
+            'new_member' => Member::where('store_id', $this->store_id)->where('created_at', '>', today())->count(),
+            'new_order' => Order::where('store_id', $this->store_id)->where('created_at', '>', today())->count(),
+        ];
+
+        $warning_num = StockWarningConfig::where('store_id', $this->store_id)->value('min_number') ?? 0;
+        $stock_warning = Product::where('store_id', $this->store_id)->where('stock', '<=', $warning_num)->count();
+
+        return success([
+            'stat' => $stat,
+            'stock_warning' => $stock_warning,
         ]);
     }
 }
--- routes/store.php
+++ routes/store.php
@@ -27,6 +27,7 @@ Route::namespace('Store')->prefix('store')->group(function () {
 
         Route::prefix('dashboard')->group(function () {
             Route::post('consume-data', 'DashboardController@consumeData');
+            Route::post('app-index', 'DashboardController@appIndex');
         });
 
         Route::prefix('member')->group(function () {

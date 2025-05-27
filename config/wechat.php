e4422e9431879c6d7a0aba02e7b37e3c6b811ec8
六元 <liuyuans@live.com>
2025-05-27 09:34:10 +0800

    config

--- config/wechat.php
+++ config/wechat.php
@@ -1,16 +1,8 @@
 <?php
 return [
-    'app_id' => 'wx7207b667f38030a3',
-    'secret' => '25f4032c97ef2e147badc6d260f92bd9',
-    'token' => 'test',
     'finance' => [
-        'app_id' => 'wxbd031528d3bdd649',
-        'secret' => '5d004367bac7eebbedd9a8f452d44656',
-        'token' => 'finance',
-    ],
-    'plus' => [
-        'app_id' => 'wxdaaf2006e8e37b39',
-        'secret' => '895465a7945e2cca05139d64eaf550f2',
-        'token' => 'finance',
-    ],
+        'app_id' => env('FINANCE_TOKEN'),
+        'secret' => env('FINANCE_APP_ID'),
+        'token' => env('FINANCE_SECRET'),
+    ]
 ];

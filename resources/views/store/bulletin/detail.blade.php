ab9283303ded9818bbc39b6e0ec898a2d7fcddbf
Liuyuan <liuyuans@live.com>
2025-06-26 15:49:13 +0800

    Bulletin

--- resources/views/store/bulletin/detail.blade.php
+++ resources/views/store/bulletin/detail.blade.php
@@ -3,7 +3,14 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta name="screen-orientation" content="portrait">
+    <meta name="x5-orientation" content="portrait">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge;chrome=1">
     <title>{{ $bulletin->title }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
